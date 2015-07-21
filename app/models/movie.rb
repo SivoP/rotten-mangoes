bf42a22e72fa041b4efe5efed98b4ded0fa5a8f6
SivoP <siv.padhy@gmail.com>
2015-07-21 04:53:23 +0000

    validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,22 @@
 class Movie < ActiveRecord::Base
+  validates :title, presence: true
+
+  validates :director, presence: true
+
+  validates :description, presence: true
+
+  validates :poster_image_url, presence: true
+
+  validates :release_date, presence: true
+
+  validate :release_date_is_in_the_future
+
+  protected
+
+  def release_date_is_in_the_future
+    if release_date.present?
+       errors.add(:release_date, "should be in future")if release_date < Date.today
+     end
+  end
 end
+
