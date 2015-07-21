aea809b86b45ca6b0c34d8e5a9652f86bfc2ffb0
SivoP <siv.padhy@gmail.com>
2015-07-21 01:58:36 +0000

    added movie model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = '984f8c046d58447b2cc7e9e6c64cc931b44a90ac054e848fd84347f86561aecca1f81964171839082b40242b5f63c9f621e041b67774bc2b77825a627c0c4542'
